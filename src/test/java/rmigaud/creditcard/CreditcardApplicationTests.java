20127dc75d86e4d62838fbbe32b2e62cf67ee2ab
Ryan Migaud <rmigaud@asu.edu>
2023-02-27 20:31:54 -0800

    initial creditcard project

---
+++ src/test/java/rmigaud/creditcard/CreditcardApplicationTests.java
@@ -0,0 +1,13 @@
+package rmigaud.creditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
